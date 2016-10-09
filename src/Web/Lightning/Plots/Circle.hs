58c01221e6df6bab2dc005b3b6577ca32452a474
Connor Moreside <cmoresid@ualberta.ca>
2016-10-08 18:48:52 -0600

    Add circle and histogram plots. Closes #6 and closes #7.

---
+++ src/Web/Lightning/Plots/Circle.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Visualize spatial points as a scatter plot.
+module Web.Lightning.Plots.Circle
+  ( CirclePlot(..)
+  , Visualization (..)
+  , circlePlot
+  ) where
+
+--------------------------------------------------------------------------------
+import           Data.Aeson
+import           Data.Default.Class
+import qualified Data.Text                         as T
+
+import qualified Web.Lightning.Routes              as R
+import           Web.Lightning.Types.Lightning     (LightningT, sendPlot)
+import           Web.Lightning.Types.Visualization (Visualization (..))
+import           Web.Lightning.Utilities
+--------------------------------------------------------------------------------
+
+-- | Circle plot parameters
+data CirclePlot =
+  CirclePlot {
+                cpConn   :: [[Double]]
+                -- ^ Matrix that defines the connectivity of the plot. The
+                -- dimensions of the matrix can be (n, n), (n, 2) or (n, 3).
+                -- Matrix can be binary or continuous valued. Links should
+                -- contain either 2 elements per link (source, target) or
+                -- 3 elements (source, target, value).
+              , cpGroup  :: Maybe [Int]
+                  -- ^ List to set colors via groups.
+              , cpColor  :: Maybe [Int]
+                -- ^ (k, 3) : List of rbg values to set colors of top-level
+                -- group, where k is the number of unique elements in the
+                -- top-level group.
+              , cpLabels :: Maybe [T.Text]
+                -- ^ List of text labels to label nodes.
+              }
+  deriving (Show, Eq)
+
+instance Default CirclePlot where
+  def = CirclePlot [[]] Nothing Nothing Nothing
+
+instance ToJSON CirclePlot where
+  toJSON (CirclePlot conn gs cs ls) =
+    omitNulls [ "links"  .= getLinks conn
+              , "nodes"  .= getNodes conn
+              , "group"  .= gs
+              , "color"  .= cs
+              , "labels" .= ls
+              ]
+
+-- | Submits a request to the specified lightning-viz server to create
+-- a scatter plot.
+--
+-- <http://lightning-viz.org/visualizations/circle/ Circle Visualization>
+circlePlot :: Monad m => T.Text
+                          -- ^ Base URL for lightning-viz server.
+                      -> CirclePlot
+                        -- ^ Circle plot to create.
+                      -> LightningT m Visualization
+                        -- ^ Transformer stack with created visualization.
+circlePlot bUrl circlePlt = do
+  viz <- sendPlot "circle" circlePlt R.plot
+  return $ viz { vizBaseUrl = Just bUrl }
---
+++ src/Web/Lightning/Plots/Histogram.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Visualize one-dimensional series data as lines.
+module Web.Lightning.Plots.Histogram
+  (
+    HistogramPlot(..)
+  , Visualization (..)
+  , histogramPlot
+  )
+  where
+
+--------------------------------------------------------------------------------
+import           Data.Aeson
+import           Data.Default.Class
+import qualified Data.Text                         as T
+
+import qualified Web.Lightning.Routes              as R
+import           Web.Lightning.Types.Lightning     (LightningT, sendPlot)
+import           Web.Lightning.Types.Visualization (Visualization (..))
+import           Web.Lightning.Utilities           (omitNulls)
+--------------------------------------------------------------------------------
+
+-- | Histogram plot parameters
+data HistogramPlot =
+  HistogramPlot { hpValues :: [Double]
+                , hpBins   :: Maybe [Double]
+                , hpZoom   :: Maybe Bool
+                }
+  deriving (Show, Eq)
+
+instance Default HistogramPlot where
+  def = HistogramPlot [] Nothing (Just True)
+
+instance ToJSON HistogramPlot where
+  toJSON (HistogramPlot vs bs z) =
+    omitNulls [ "values" .= vs
+              , "bins"   .= bs
+              , "zoom"   .= z
+              ]
+
+-- | Submits a request to the specified lightning-viz server to create a plot
+-- to histogram.
+--
+-- <http://lightning-viz.org/visualizations/histogram/ HistogramPlot Visualization>
+histogramPlot :: Monad m => T.Text
+                            -- ^ Base URL for lightning-viz server.
+                         -> HistogramPlot
+                            -- ^ Histogram plot to create.
+                         -> LightningT m Visualization
+                            -- ^ Transformer stack with created visualization.
+histogramPlot bUrl histPlt = do
+  viz <- sendPlot "histogram" histPlt R.plot
+  return $ viz { vizBaseUrl = Just bUrl }
